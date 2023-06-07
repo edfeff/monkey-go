6cc21ecc3dcbccbc31143b97134132559a07a339
wpp <pengpeng.wang@gemii.cc>
2023-06-07 11:57:06 +0800

    feat: hash mk

---
+++ monkeys/hashs.mk
@@ -0,0 +1,12 @@
+//hash测试示例
+let people =[{"name":"Alice","age":24},{"name":"Anna","age":28}];
+
+let getName = fn(person){person["name"];}
+
+puts("-->");
+
+puts(people[0]["name"]);
+
+puts(people[1]["age"]);
+
+puts(getName(people[1]));
